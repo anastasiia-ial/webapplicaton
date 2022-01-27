fdf461ab40104f54872d7835d96e9a0e023e4687
Anastasiia <filina0906@gamil.com>
2022-01-27 10:03:09 +0300

    Создание карточек статуса

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html LANG="en">
 <head>
     <title>WEB APPLICATON</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 <?php echo '<p>Hello World</p>'; ?>
